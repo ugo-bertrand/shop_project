ffe496757a93701fb46c5ba1022577c510840e8f
ugo-bertrand <ugo.bertrand@epitech.eu>
2022-11-18 10:28:21 +0100

    controller and gitignore

---
+++ API/controller/role.controller.js
@@ -0,0 +1,33 @@
+const db = require("../model/db.js");
+
+//role controller
+
+//method to create a role in the database
+exports.createRole = (req,res) =>{
+    //TODO
+}
+
+//method to return all roles in the database
+exports.findAll = (req,res) =>{
+    //TODO
+}
+
+//method to return a role bases on its ID
+exports.findById = (req,res) =>{
+    //TODO
+}
+
+//method to return a role bases on its name
+exports.findByRoleName = (req,res) =>{
+    //TODO
+}
+
+//method to change the role's information
+exports.updateRoleById = (req,res) =>{
+    //TODO
+}
+
+//method to delete a role
+exports.deleteById = (req,res) =>{
+    //TODO
+}
---
+++ API/controller/user.controller.js
@@ -0,0 +1,49 @@
+const db = require("../model/db.js");
+
+//user controller
+
+//method to create a user in the database
+exports.createUser = (req,result) => {
+    //TODO
+}
+
+//method to return all users in the database
+exports.findAll = (req,res) =>{
+    //TODO
+}
+
+//method to return a user based on its ID
+exports.findById = (req,res) =>{
+    //TODO
+}
+
+//method to return all users based on its role name
+exports.findByRoleName = (req,res) =>{
+    //TODO
+}
+
+//method to return a user based on its email
+exports.findByEmail = (req,res) =>{
+    //TODO
+}
+
+//method to return a user bases on the name and the firstname
+exports.findByName = (req,res) =>{
+    //TODO
+}
+
+//method to activate/deactivate an account
+exports.setEnable = (req,res) =>{
+    //TODO
+}
+
+//method to change the user's information
+exports.updateUserById = (req,res) =>{
+    //TODO
+}
+
+//method to delete a user
+exports.deleteById = (req,res) =>{
+    //TODO
+}
+
