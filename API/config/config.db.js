e2f98e7ceb3962ce183d65636858562f9d38a5e5
ugo-bertrand <ugo.bertrand@epitech.eu>
2022-12-14 12:19:21 +0100

    reset

--- API/config/config.db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    HOST: process.env.DATABASE_HOST,
-    USER: process.env.DATABASE_USER,
-    PASSWORD: process.env.DATABASE_PASSWORD,
-    DB: process.env.DATABASE_NAME
-};
