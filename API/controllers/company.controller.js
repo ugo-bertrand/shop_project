e1cfce1fd33c48e6b2d53087cf2dc27d9287e36c
ugo-bertrand <ugo.bertrand@epitech.eu>
2022-12-16 14:36:46 +0100

    company controller

--- API/controllers/company.controller.js
+++ API/controllers/company.controller.js
@@ -1,1 +1,43 @@
 const db = require("../db.js");
+const dateFile = require("../date.js");
+
+exports.createCompany = (req,result) => {
+    var date = dateFile.GetDate(new Date());
+    if(Object.keys(req.body).length === 0){
+        result.status(404).send({
+            message:"Le contenu ne doit pas être vide."
+        });
+        console.log(date + " : Le contenu est vide la requête ne peut pas être envoyer (api/companies/addCompany).");
+        return;
+    }
+    const company = {
+        ...req.body
+    };
+    db.query("INSERT INTO companies SET ?",company,(error,res) => {
+        if(error){
+            result.status(500).send({
+                message:"Une erreur est survenue lors de l'envoie de la requête."
+            });
+            console.log(date + " : Une erreur est survenue lors de l'envoie de la requête (api/companies/addCompany).");
+            console.log(error);
+            return;
+        }
+        if(!res){
+            result.status(200).send({
+                message:"L'entreprise avec le nom"
+            })
+        }
+    })
+}
+
+exports.findAll = (req,result) => {
+
+}
+
+exports.updateById = (req,result) => {
+
+}
+
+exports.deleteById = (req,result) => {
+
+}
