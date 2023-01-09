0f50c46d3eaff3ccc842955cb3551194b0d99f3a
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-01-09 12:03:17 +0100

    app config

--- API/app.js
+++ API/app.js
@@ -21,12 +21,18 @@ var corsOption = {
 app.use(cors(corsOption));
 
 var roleRoutes = require("./routes/role.routes.js");
+var userRoutes = require("./routes/user.routes.js");
+var companyRoutes = require("./routes/company.routes.js");
+var categoryRoutes = require("./routes/role.routes.js");
 
 
 app.use(helmet());
 app.use(express.json());
-//app.use(cookieParser());
+app.use(cookieParser());
 
 app.use("/api/roles", roleRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/companies", companyRoutes);
+app.use("/api/categories", categoryRoutes);
 
 module.exports = app;
